881bab61a979bfbe1589b0659747e76381ce07b8
Andr.dre <kolsergienko@gmail.com>
2022-12-27 02:46:49 +0300

    added TodoList component

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,11 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { TodoListItem } from "../TodoListItem";
+import "./TodoList.css";
 
 export class TodoList extends Component {
 	render() {
+		const { todoItems } = this.props;
 		return (
-			<ul>
-				<li>Drink Coffee</li>
-				<li>Do Coding</li>
+			<ul className="list-group todo-list">
+				{todoItems.map((item) => {
+					const { id, ...itemOthers } = item;
+					return (
+						<li
+							key={id}
+							className="list-group-item"
+						>
+							<TodoListItem
+								{...itemOthers}
+								onDelete={() => this.props.onDelete(id)}
+								onToggleCompleted={() => this.props.onToggleCompleted(id)}
+								onToggleImportant={() => this.props.onToggleImportant(id)}
+							/>
+						</li>
+					);
+				})}
 			</ul>
 		);
 	}
