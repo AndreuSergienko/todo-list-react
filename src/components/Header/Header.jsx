376083b568d1c0f5531af5b11cfb32d49d8f74f3
Andr.dre <kolsergienko@gmail.com>
2022-12-27 02:47:31 +0300

    modified Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./Header.css";
 
 export class Header extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Todo-List</h1>
-      </div>
-    )
-  }
+	render() {
+		const { toDo, done } = this.props;
+
+		return (
+			<div className="app-header">
+				<h1 className="app-header-logo">Todo-List</h1>
+				<span className="app-header-counter">
+					{toDo} more to do, {done} done
+				</span>
+			</div>
+		);
+	}
 }
